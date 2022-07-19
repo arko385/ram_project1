fe888c63790542f8820546632e523efa8c074294
arko385 <90547765+arko385@users.noreply.github.com>
2022-07-19 15:19:50 +0530

    Indexer uploaded

---
+++ ram_project1/InDexers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ram_project1
+{
+     class emp
+    {
+        int[] age=new int[3];
+        string[] num = new string[3];
+        public int this[int index]
+        {
+            set
+            {
+                if(index>=0 && index<age.Length)
+                {
+                    if(value>0)
+                    age[index]=value;
+                    else
+                    Console.WriteLine("Enter a valid age !");
+
+                }
+                else
+                Console.WriteLine("Enter a valid Index !");
+
+            }
+            get
+            {  
+            return age[index];
+            }
+        }
+
+     
+
+    }
+
+    class program
+    {
+        static void Main()
+        {
+            emp a=new emp();
+            a[0] = 10;
+           
+            Console.WriteLine(a[0]);
+
+        }
+    }
+}

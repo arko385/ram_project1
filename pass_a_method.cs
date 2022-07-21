c8e9384f2ab1b394e735923699c0fc4030ea9b2e
arko385 <90547765+arko385@users.noreply.github.com>
2022-07-21 12:09:44 +0530

    Pass a method into another method using delegates

---
+++ pass_a_method.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ram_project1
+{
+    public delegate int del(int a);
+    class program
+    {
+        public static int pow1(int a)
+        {
+             int k, r=1;
+            k = a;
+            while(k>0)
+            {
+                r = r * a;
+                k--;
+            }
+            return r;
+        }
+        public  static void show(del d,int a)
+        {
+            int z = d(a) + a;
+            Console.WriteLine(z);
+        }
+        static void Main()
+        {
+            del q = new del(pow1);
+            program.show(q, 3);
+        }
+    }
+   
+}
